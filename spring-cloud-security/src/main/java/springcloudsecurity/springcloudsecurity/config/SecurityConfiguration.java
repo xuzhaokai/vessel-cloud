a87c5661748d0d2a9e251881edcd19e3c1bd1928
Leon Yu <leon.yu@oocl.com>
2019-03-29 02:21:13 +0800

    add define security login page

--- spring-cloud-security/src/main/java/springcloudsecurity/springcloudsecurity/config/SecurityConfiguration.java
+++ spring-cloud-security/src/main/java/springcloudsecurity/springcloudsecurity/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package springcloudsecurity.springcloudsecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import springcloudsecurity.springcloudsecurity.service.UserDetailService;
@@ -23,5 +24,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/","/hello","/login2","/aouth").permitAll() //指定不需要验证的页面，其他的默认会跳转到登录页
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()  //支持表单提交
+            .loginPage("/login2").permitAll()  //自定义登录页面
+            .failureForwardUrl("/error")   //自定也错误
+            .loginProcessingUrl("/login")  //提交action  也就是form表单中的action  login会调用security的登录不用自己实现
+            .successForwardUrl("/hello")  //登录成功页面
+            .and().logout()
+            .permitAll();
+
+        //        System.out.println(http.toString());
     }
 }
--- spring-cloud-security/src/main/java/springcloudsecurity/springcloudsecurity/controller/OAuthController.java
+++ spring-cloud-security/src/main/java/springcloudsecurity/springcloudsecurity/controller/OAuthController.java
@@ -1,6 +1,7 @@
 package springcloudsecurity.springcloudsecurity.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,11 +12,11 @@ import javax.servlet.http.HttpServletResponse;
  * @Date: Create in 1:32 AM 3/29/2019
  * @Modified By:
  */
+@RestController
 public class OAuthController {
 
-    @RequestMapping("/hello")
-    String home(HttpServletRequest req,HttpServletResponse res) {return "hello world";
-    }
+    @RequestMapping("/")
+    public String hello(HttpServletRequest req,HttpServletResponse res) {return "hello world";}
 
 
 }
